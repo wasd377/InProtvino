d3fb418a22c1eab3670fe8a678fb71dcf6b518fd
Антон <anton@workout.su>
2023-10-17 17:12:01 +0300

    Initial Commit

---
+++ Protvino/ProtvinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtvinoApp.swift
+//  Protvino
+//
+//  Created by Natalia D on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProtvinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
